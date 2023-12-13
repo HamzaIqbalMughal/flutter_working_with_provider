4ddfdc40ce59964efc2c008ab9b3bda2a7f6ccb9
HamzaIqbalMughal <humza.iqbal.hi@gmail.com>
2023-12-14 01:19:12 +0500

    added LogInScreen with provider Completed.

--- lib/provider/auth_provider.dart
+++ lib/provider/auth_provider.dart
@@ -2,20 +2,42 @@ import 'package:flutter/foundation.dart';
 import 'package:http/http.dart';
 
 class AuthProvider with ChangeNotifier{
+
+  bool _loading = false;
+  bool get loading => _loading;
+
+  setLoading(bool value){
+    _loading = value;
+    notifyListeners();
+  }
+
+  bool _passwordVisibility = true;
+  bool get passwordVisibility => _passwordVisibility;
+
+  toggleVisibilty(){
+    _passwordVisibility = !_passwordVisibility;
+    notifyListeners();
+  }
+
   void login(String email, String password) async{
+
+    setLoading(true);
     try{
       Response response = await post(
           Uri.parse('https://reqres.in/api/login'),
         body: {
-            "email": email,
-          "password": password
+            'email': email,
+          'password': password
         }
       );
 
       if(response.statusCode == 200){
         print('successful');
+        setLoading(false);
       }else{
         print('Unsuccessful');
+        setLoading(false);
+
       }
     }catch(e){
       print(e.toString());
--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_working_with_provider/provider/auth_provider.dart';
+import 'package:provider/provider.dart';
 
 class LogInScreen extends StatefulWidget {
   const LogInScreen({super.key});
@@ -16,6 +18,7 @@ class _LogInScreenState extends State<LogInScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final authProvider = Provider.of<AuthProvider>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('LogIn'),
@@ -34,27 +37,33 @@ class _LogInScreenState extends State<LogInScreen> {
             ),
             SizedBox(height: 20),
             TextFormField(
-              obscureText: true,
+              obscureText: authProvider.passwordVisibility,
               controller: _passwordController,
               decoration: InputDecoration(
                 hintText: 'Password',
                 suffixIcon: InkWell(
                   onTap: (){
-
+                    authProvider.toggleVisibilty();
                   },
-                  child: Icon(Icons.visibility),
+                  child: authProvider.passwordVisibility ? Icon(Icons.visibility) : Icon(Icons.visibility_off),
                 ),
               ),
             ),
             SizedBox(height: 20),
-            Container(
-              height: 50,
-              decoration: BoxDecoration(
-                color: Colors.blue,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Center(
-                child: Text('LogIn'),
+            InkWell(
+              onTap: (){
+                authProvider.login(_emailController.text.toString(), _passwordController.text.toString());
+              },
+              child: Container(
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: authProvider.loading ? CircularProgressIndicator(color: Colors.white,) : Text('LogIn'),
+
+                ),
               ),
             )
           ],
